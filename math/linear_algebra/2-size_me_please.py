1de3e54c7738023c8a31a0e416d026761d2ef4d2
l-isaro <l.isaro@alustudent.com>
2025-01-24 10:43:37 +0200

    Add flip me over file

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+
+"""
+This module provides functions for calculating matrix shapes.
+"""
+
 def matrix_shape(matrix):
     """
     Calculate the shape of a matrix.
--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,7 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+This module provides functions for matrix operations.
+
+Currently, it includes a function to transpose a matrix.
+"""
+
 def matrix_transpose(matrix):
-    transpose = []
-    for i in range(len(matrix)):
-        for j in range(len(matrix)):
+    """
+    Transpose a matrix.
+
+    Args:
+        matrix (list): A list of lists representing the matrix.
+
+    Returns:
+        list: A list representing the transposed matrix.
+    """
+    transpose = [[0 for _ in range(len(matrix))] for _ in range(len(matrix[0]))]
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix)):
+            print(i, j)
             transpose[j][i] = matrix[i][j] 
     return transpose
 
